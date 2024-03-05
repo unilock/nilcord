e154a448fbf74b373d221366edef765daeb11b8b
unilock <unilock@fennet.rentals>
2024-03-05 17:38:57 -0500

    logging chat messages (untested: needs dedicated server)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/nilcord/NilcordPremain.java
+++ src/main/java/cc/unilock/nilcord/NilcordPremain.java
@@ -2,6 +2,7 @@ package cc.unilock.nilcord;
 
 import cc.unilock.nilcord.transformer.EntityPlayerTransformer;
 import cc.unilock.nilcord.transformer.EntityServerPlayerTransformer;
+import cc.unilock.nilcord.transformer.NetServerHandlerTransformer;
 import nilloader.api.ClassTransformer;
 import nilloader.api.ModRemapper;
 import nilloader.api.NilLogger;
@@ -15,5 +16,6 @@ public class NilcordPremain implements Runnable {
 
 		ClassTransformer.register(new EntityPlayerTransformer());
 		ClassTransformer.register(new EntityServerPlayerTransformer());
+		ClassTransformer.register(new NetServerHandlerTransformer());
 	}
 }
---
+++ src/main/java/cc/unilock/nilcord/transformer/NetServerHandlerTransformer.java
@@ -0,0 +1,30 @@
+package cc.unilock.nilcord.transformer;
+
+import cc.unilock.nilcord.NilcordPremain;
+import net.minecraft.network.NetServerHandler;
+import net.minecraft.network.packet.Packet3Chat;
+import nilloader.api.lib.mini.MiniTransformer;
+import nilloader.api.lib.mini.PatchContext;
+import nilloader.api.lib.mini.annotation.Patch;
+
+@Patch.Class("net.minecraft.network.NetServerHandler")
+public class NetServerHandlerTransformer extends MiniTransformer {
+	@Patch.Method("handleChat(Lnet/minecraft/network/packet/Packet3Chat;)V")
+	public void patchHandleChat(PatchContext ctx) {
+		ctx.search(
+				GETSTATIC("net/minecraft/network/NetServerHandler", "logger", "Ljava/util/logging/Logger")
+		).jumpBefore();
+
+		ctx.add(
+				ALOAD(0),
+				ALOAD(1),
+				INVOKESTATIC("cc/unilock/nilcord/transformer/NetServerHandlerTransformer$Hooks", "chatMessage", "(Lnet/minecraft/network/NetServerHandler;Lnet/minecraft/network/packet/Packet3Chat;)V")
+		);
+	}
+
+	public static class Hooks {
+		public static void chatMessage(NetServerHandler netServerHandler, Packet3Chat packet) {
+			NilcordPremain.LOGGER.info("Chat Message: \""+packet.message+"\" from "+netServerHandler.playerEntity.username);
+		}
+	}
+}
