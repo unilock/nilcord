19a0939d21a6b09ed656b1c351b1168287b1bcf5
unilock <unilock@fennet.rentals>
2025-06-26 17:50:40 -0400

    adjust CrashReport injection point
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/nilcord/mixin/early/minecraft/CrashReportMixin.java
+++ src/main/java/cc/unilock/nilcord/mixin/early/minecraft/CrashReportMixin.java
@@ -5,12 +5,14 @@ import net.minecraft.crash.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.File;
 
 @Mixin(CrashReport.class)
 public class CrashReportMixin {
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void writeToFile(String description, Throwable cause, CallbackInfo ci) {
+    @Inject(method = "saveToFile", at = @At(value = "NEW", target = "(Ljava/io/File;)Ljava/io/FileWriter;"))
+    private void saveToFile(File file, CallbackInfoReturnable<Boolean> cir) {
         NilcordPremain.listener.serverCrash((CrashReport) (Object) this);
     }
 }
