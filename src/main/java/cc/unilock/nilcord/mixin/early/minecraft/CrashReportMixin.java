a030dc78fff38ba5093d0c771bec43154f90d52b
unilock <unilock@fennet.rentals>
2025-08-11 07:03:59 -0400

    fix server crash detection
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/nilcord/mixin/early/minecraft/CrashReportMixin.java
+++ src/main/java/cc/unilock/nilcord/mixin/early/minecraft/CrashReportMixin.java
@@ -5,12 +5,12 @@ import net.minecraft.crash.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CrashReport.class)
 public class CrashReportMixin {
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void writeToFile(String description, Throwable cause, CallbackInfo ci) {
+    @Inject(method = "saveToFile", at = @At(value = "NEW", target = "(Ljava/io/File;)Ljava/io/FileOutputStream;", remap = false))
+    private void saveToFile(CallbackInfoReturnable<Boolean> cir) {
         NilcordPremain.listener.serverCrash((CrashReport) (Object) this);
     }
 }
