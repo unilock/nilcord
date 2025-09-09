286bc14b8613e58874f42560e6245ee81c3e6822
unilock <katherines1337@outlook.com>
2025-09-09 07:40:14 -0400

    allow using skin_model in webhook avatar

--- src/main/java/cc/unilock/nilcord/util/SkinUtils.java
+++ src/main/java/cc/unilock/nilcord/util/SkinUtils.java
@@ -1,45 +1,30 @@
 package cc.unilock.nilcord.util;
 
 import cc.unilock.nilcord.NilcordPremain;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
-import com.mojang.authlib.properties.Property;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static cc.unilock.nilcord.NilcordPremain.CONFIG;
 
 public class SkinUtils {
-    private static final LoadingCache<String, Map<MinecraftProfileTexture.Type, MinecraftProfileTexture>> skinCache = CacheBuilder.newBuilder().expireAfterAccess(15L,TimeUnit.SECONDS).build(new CacheLoader<>() {
-        @NotNull
-        public Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> load(@NotNull String string) {
-            GameProfile gameProfile = new GameProfile(null, "dummy_mcdummyface");
-            gameProfile.getProperties().put("textures", new Property("textures", string, ""));
+    public static Skin getSkin(GameProfile profile) {
+        Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = NilcordPremain.server.getSessionService().getTextures(profile, false);
 
-            try {
-                return NilcordPremain.server.getSessionService().getTextures(gameProfile, false);
-            } catch (Throwable var4) {
-                return ImmutableMap.of();
-            }
+        if (map.containsKey(MinecraftProfileTexture.Type.SKIN)) {
+            MinecraftProfileTexture skin = map.get(MinecraftProfileTexture.Type.SKIN);
+           return new Skin(
+                   skin.getUrl().substring(38),
+                   skin.getMetadata("model")
+           );
+        } else {
+            return new Skin(
+                    CONFIG.formatting.discord.webhook.default_skin_id.value(),
+                    CONFIG.formatting.discord.webhook.default_skin_model.value()
+            );
         }
-    });
-
-    public static Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> getTextures(GameProfile profile) {
-        Property property = Iterables.getFirst(profile.getProperties().get("textures"), null);
-        return property == null ? ImmutableMap.of() : skinCache.getUnchecked(property.getValue());
     }
 
-    public static String getSkin(GameProfile profile) {
-        Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = getTextures(profile);
-        return map.containsKey(MinecraftProfileTexture.Type.SKIN)
-                ? map.get(MinecraftProfileTexture.Type.SKIN).getUrl().substring(38)
-                : CONFIG.formatting.discord.webhook.default_skin_id.value();
-    }
+    public record Skin(String id, String model) {}
 }
